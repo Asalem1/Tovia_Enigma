aee484630d8513fd8aa1834545256979251ae16e
Ariel <Ariel.Salem1989@gmail.com>
2017-08-03 09:05:51 -0700

    adding initial styles

--- src/components/app.js
+++ src/components/app.js
@@ -8,8 +8,66 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Testing hello world!</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xs-5 offset-xs-4 tovia-container">
+            <header className="row">
+              <p className="tovia-header">Tovia's Enigma</p>
+            </header>
+            <div>
+              <form>
+                <div className="row name-row">
+                  <div className="col-xs-2">
+                    <img src="https://www.random.org/analysis/randbitmap-rdo.png" className="image-icon" />
+                  </div>
+                  <div className="col-xs-5">
+                    <div className="row">
+                      <label htmlFor="name">Name *</label>
+                    </div>
+                    <div className="row">
+                      <input placeholder="Name *"/>
+                    </div>
+                  </div>
+                </div>
+                <div className="row message-row">
+                  <div className="row">
+                    <p>Message * </p>
+                  </div>
+                  <div className="row">
+                    <input placeholder="Message *"/>
+                  </div>
+                </div>
+                <div className="row expiration-row">
+                  <div className="row">
+                    <p>Expiration date * </p>
+                  </div>
+                  <div className="row">
+                    <input placeholder="Expiration date *"/>
+                  </div>
+                </div>
+                <br />
+                <div className="row">
+                  <div className="col-sm-2">
+                  <button>ENCRYPT</button>
+                  </div>
+                  <div className="col-sm-2 offset-sm-3">
+                  <button>DECRYPT</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="row passphrase-component">
+          <div className="row">
+            <div>
+              <p>Your Passphrase - <a href="#"> Rp9Vz </a></p>
+            </div>
+          </div>
+          <div className="row">
+            <a href="#">Generate new Passphrase</a>
+          </div>
+        </div>
       </div>
     )
   }
